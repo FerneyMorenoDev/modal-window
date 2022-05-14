f141661d2f4dc86545ee3e6b62826e002b72c558
Ferney22moreno <fam.grafico@hotmail.com>
2022-05-14 21:15:57 +0200

    added key 'cancel' behavior

--- script.js
+++ script.js
@@ -3,32 +3,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalButtons = document.querySelectorAll('.show-modal');
-let isModalOpen = false;
 
 const showModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-  isModalOpen = true;
 };
 
 const hideModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-  isModalOpen = false;
 };
 
 openModalButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    showModal();
-  });
+  btn.addEventListener('click', showModal);
 });
 
-closeModalBtn.addEventListener('click', () => {
-  hideModal();
-});
+closeModalBtn.addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
 
 window.addEventListener('keydown', ({ key: keyName }) => {
-  if (keyName === 'Escape' && isModalOpen) {
+  if (keyName === 'Escape' && !modal.classList.contains('hidden')) {
     hideModal();
   }
 });
